c6f9e3b5b25abb4eee4b2b169637e8a6d023cb44
Zach Upstone <zu213@bath.ac.uk>
2025-07-31 08:16:10 +0100

    Added server logs

--- api.py
+++ api.py
@@ -15,25 +15,28 @@ from Tree_Image_Generator_opts import bicycle_gan_opts
 
 app = FastAPI()
 
+count = 0
+
 # Load model once on startup
 opt = bicycle_gan_opts('high_ren')
 model = create_model(opt)
 model.setup(opt)
+print("Model setup successfully.")
 
 # POST endpoint
 @app.post("/generate/")
 async def generate(sketch: UploadFile = File(...), style: UploadFile = File(...)):
-    sketch_image = Image.open(BytesIO(await sketch.read()))
-    style_image = Image.open(BytesIO(await style.read()))
+  sketch_image = Image.open(BytesIO(await sketch.read()))
+  style_image = Image.open(BytesIO(await style.read()))
 
-    outputs, _ = generate_images(model, sketch_image, style_image)
+  outputs, _ = generate_images(model, sketch_image, style_image)
 
-    print(outputs)
-    def image_to_base64(img):
-        buf = BytesIO()
-        img.save(buf, format="PNG")
-        return base64.b64encode(buf.getvalue()).decode('utf-8')
+  def image_to_base64(img):
+    buf = BytesIO()
+    img.save(buf, format="PNG")
+    return base64.b64encode(buf.getvalue()).decode('utf-8')
 
-    return {
-        "images": [image_to_base64(img) for img in outputs]
-    }
+  print("Requested successfully, count since last restart: ", count)
+  return {
+    "images": [image_to_base64(img) for img in outputs]
+  }
